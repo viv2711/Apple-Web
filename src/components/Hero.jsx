1d7df97edc46673c5c4b9dffd6674a0d32aef777
viv2711 <vksingh27112001@gmail.com>
2024-09-07 18:19:51 +0530

    add iPhone video for different window width

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import {heroVideo, smallHeroVideo} from '../utils/index.js'
 function Hero() {
+  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo);
+  const handleVideoSrcSet = () => {
+    if(window.innerWidth < 760){
+      setVideoSrc(smallHeroVideo);
+    }
+    else{
+      setVideoSrc(heroVideo);
+    }
+  }
+  useEffect(() => {
+    window.addEventListener('resize', handleVideoSrcSet);
+    return () => {
+      window.removeEventListener('resize', handleVideoSrcSet);
+    }
+  })
+  useGSAP(() => {
+    gsap.to('#hero', {
+      opacity: 1,
+      delay: 1,
+    })
+  }, [])
   return (
     <section className='w-full nav-height bg-black realtive'>
       <div className="h-5/6 w-full flex-center flex-col">
-        <p className='hero-title'>iPhone 15 Pro</p>
+        <p id='hero' className='hero-title'>iPhone 15 Pro</p>
+        <div className="md:w-10/12 w-9/12">
+          <video className='pointer-event-none'autoPlay muted playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type='video/mp4'/>
+          </video>
+        </div>
       </div>
     </section>
   )
