212f3e9cb6c61d096a9d06da61121063c152ab1e
viv2711 <vksingh27112001@gmail.com>
2024-09-07 18:53:09 +0530

    add buy button with animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,14 @@ function Hero() {
   useGSAP(() => {
     gsap.to('#hero', {
       opacity: 1,
-      delay: 1,
+      delay: 2,
+    })
+  }, [])
+  useGSAP(() => {
+    gsap.to('#cta', {
+      y: -50,
+      opacity: 1,
+      delay: 2,
     })
   }, [])
   return (
@@ -29,10 +36,14 @@ function Hero() {
       <div className="h-5/6 w-full flex-center flex-col">
         <p id='hero' className='hero-title'>iPhone 15 Pro</p>
         <div className="md:w-10/12 w-9/12">
-          <video className='pointer-event-none'autoPlay muted playsInline={true} key={videoSrc}>
+          <video className='pointer-event-none' autoPlay muted playsInline key={videoSrc}>
             <source src={videoSrc} type='video/mp4'/>
           </video>
         </div>
+      </div>
+      <div id='cta' className='flex flex-col items-center opacity-0 translate-y-20'>
+        <a href="#highlights" className='btn'>Buy</a>
+        <p className='font-normal text-xl'>From $199/month or $999</p>
       </div>
     </section>
   )
