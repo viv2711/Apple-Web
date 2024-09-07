23877d73a5d554abac6bd9d5d32cb899d33880a8
viv2711 <vksingh27112001@gmail.com>
2024-09-07 21:25:35 +0530

    add highlights section layout

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
-
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { rightImg, watchImg } from '../utils/index.js'
 function Highlights() {
+  useGSAP(() => {
+    gsap.to('#title', {
+      opacity: 1, y: 0
+    })
+    gsap.to('.link', {
+      opacity: 1, y: 0, stagger: 0.5, duration:1
+    })
+  })
   return (
-    <div>
-      Highlights
-    </div>
+    <section id='highlights' className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+      <div className="screen-max-width">
+        <div className='md:flex mb-12 w-full items-end justify-between'>
+          <h1 id='title' className='section-heading'>Get the highlights.</h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className='link'>
+              Watch the film
+              <img src={watchImg} alt="watchImg" className='ml-2' />
+            </p>
+            <p className='link'>
+              Watch the event
+              <img src={rightImg} alt="watchImg" className='ml-2' />
+            </p>
+          </div>
+        </div>
+        
+      </div>
+
+    </section>
   )
 }
 
