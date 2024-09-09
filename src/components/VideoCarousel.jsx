b5e00464111b63c161c6e98861286203f98b3f64
viv2711 <vksingh27112001@gmail.com>
2024-09-09 05:46:47 +0530

    add video

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -4,5 +4,45 @@ import { playImg, replayImg, pauseImg } from "../utils/index.js";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
+const VideoCarousel = () => {
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
+
+  const [video, setVideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false
+  });
+  const {isEnd, startPlay, videoId, isLastVideo, isPlaying} = video;
+  return (
+    <>
+      <div className="flex items-center ">
+        {highlightsSlides.map((list, i) => (
+          <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+            <div className="video-carousel_container">
+              <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                <video id="video" autoPlay playsInline muted key={list.id}>
+                  <source src={list.video} type="video/mp4" />
+                </video>
+              </div>
+              <div className="absolute top-12 left-[5%] z-10">
+                  {list.textLists.map((text) => (
+                    <p key={text} className="md:text-2xl text-xl font-medium">
+                      {text}
+                    </p>
+                  ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default VideoCarousel;
