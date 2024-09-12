36ec4dca84db382748e8a303d473d2a0ccca81ef
viv2711 <vksingh27112001@gmail.com>
2024-09-12 16:04:30 +0530

    add processor info section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Model from "./components/Model"
-import * as Sentry from '@sentry/react'
+import Model from "./components/Model";
+import * as Sentry from "@sentry/react";
 import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
 const App = () => {
-
   return (
     <main className="bg-black">
-     <Navbar/>
-     <Hero/>
-     <Highlights/>
-     <Model />
-     <Features />
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
     </main>
-  )
-}
+  );
+};
 
 export default Sentry.withProfiler(App);
